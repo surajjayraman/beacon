8e951cd905979d07ddca33caa40e2fa3865e413b
surajjayraman <suraj.jayraman@gmail.com>
2024-08-29 14:34:08 -0400

    Update LeftSideBar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
-  )
-}
+    <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
+      LeftSideBar
+    </div>
+  );
+};
 
-export default LeftSideBar
+export default LeftSideBar;
