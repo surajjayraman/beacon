a91f27517ae83d0c8213a72d121e2493f0ad5fdd
surajjayraman <suraj.jayraman@gmail.com>
2024-08-30 09:57:58 -0400

    Update LeftSideBar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,13 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const LeftSideBar = () => {
   return (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
-        <Image src="/assets/christmas.jpg" alt="logo" width={100} height={100} />
+        <Image
+          src="/assets/christmas.jpg"
+          alt="logo"
+          width={100}
+          height={100}
+        />
       </Link>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 items-center text-light-1">
+          <Link href={`/`}>
+            <Image
+              src="/assets/Andrew.jpg"
+              alt="profile photo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+          </Link>
+          <p className="text-small-bold"></p>
+        </div>
+        <div className="flex text-light-1 justify-between">
+          <div className="flex flex-col items-center">
+            <p className="text-base-bold">10</p>
+            <p className="text-tiny-medium">Posts</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-base-bold">40</p>
+            <p className="text-tiny-medium">Followers</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-base-bold">2</p>
+            <p className="text-tiny-medium">Following</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
