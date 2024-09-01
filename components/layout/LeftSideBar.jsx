5b8ae706086d018d93c7fc4091e48cd38619603a
surajjayraman <suraj.jayraman@gmail.com>
2024-09-01 09:40:21 -0400

    Add api user route

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -8,7 +8,7 @@ import { dark } from "@clerk/themes";
 
 const LeftSideBar = () => {
   const { user, isLoaded } = useUser();
-  console.log("from leftsidebar:", user);
+  // console.log("from leftsidebar:", user);
   return (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
