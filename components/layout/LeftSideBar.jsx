5e18b21ac074502ff824213406b732363167cbc2
surajjayraman <suraj.jayraman@gmail.com>
2024-09-01 14:29:06 -0400

    Update component LeftSideBar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,62 +1,80 @@
 "use client";
 
+import { UserButton, useUser } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./Menu";
-import { UserButton, useUser } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import { useEffect, useState } from "react";
+import Loader from "@components/Loader";
 
 const LeftSideBar = () => {
   const { user, isLoaded } = useUser();
-  // console.log("from leftsidebar:", user);
-  return (
+
+  const [loading, setLoading] = useState(true);
+
+  const [userData, setUserData] = useState({});
+
+  const getUser = async () => {
+    const response = await fetch(`/api/user/${user.id}`);
+    const data = await response.json();
+    setUserData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, [user]);
+
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
-        <Image
-          src="/assets/christmas.jpg"
-          alt="logo"
-          width={100}
-          height={100}
-        />
+        <Image src="/assets/logo.png" alt="logo" width={200} height={200} />
       </Link>
+
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2 items-center text-light-1">
-          <Link href={`/`}>
+          <Link href={`/profile/${userData._id}/posts`}>
             <Image
-              src="/assets/Andrew.jpg"
+              src={userData?.profilePhoto}
               alt="profile photo"
               width={50}
               height={50}
               className="rounded-full"
             />
           </Link>
-          <p className="text-small-bold">Little Lemon</p>
+          <p className="text-small-bold">
+            {userData?.firstName} {userData?.lastName}
+          </p>
         </div>
         <div className="flex text-light-1 justify-between">
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">10</p>
+            <p className="text-base-bold">{userData?.posts?.length}</p>
             <p className="text-tiny-medium">Posts</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">40</p>
+            <p className="text-base-bold">{userData?.followers?.length}</p>
             <p className="text-tiny-medium">Followers</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">2</p>
+            <p className="text-base-bold">{userData?.following?.length}</p>
             <p className="text-tiny-medium">Following</p>
           </div>
         </div>
       </div>
+
       <hr />
+
       <Menu />
 
       <hr />
 
       <div className="flex gap-4 items-center">
-        <UserButton
-          appearance={{ baseTheme: dark }}
-          afterSignOutUrl="/sign-in"
-        />
+        <UserButton appearance={{ baseTheme: dark }} afterSignOutUrl="/sign-in"/>
         <p className="text-light-1 text-body-bold">Manage Account</p>
       </div>
     </div>
