368df6e40a9317891ff64b9d61f241bad3d2342d
surajjayraman <suraj.jayraman@gmail.com>
2024-09-01 09:38:40 -0400

    Update LeftSideBar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,10 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./Menu";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 const LeftSideBar = () => {
+  const { user, isLoaded } = useUser();
+  console.log("from leftsidebar:", user);
   return (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
