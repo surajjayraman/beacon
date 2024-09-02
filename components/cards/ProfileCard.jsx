9c2f36d3808cae368d47404453b48758dc84747f
surajjayraman <suraj.jayraman@gmail.com>
2024-09-02 18:14:37 -0400

    Update ProfileCard component

--- components/cards/ProfileCard.jsx
+++ components/cards/ProfileCard.jsx
@@ -32,7 +32,7 @@ const ProfileCard = ({ userData, activeTab }) => {
   }, [user]);
 
   const isFollowing = userInfo?.following?.find(
-    (item) => item._id === userData._id
+    (item) => item._id === userData?._id
   );
 
   const handleFollow = async () => {
@@ -56,7 +56,7 @@ const ProfileCard = ({ userData, activeTab }) => {
       <div className="flex justify-between items-start">
         <div className="flex gap-5 items-start">
           <Image
-            src={userData.profilePhoto}
+            src={userData?.profilePhoto}
             alt="profile photo"
             width={100}
             height={100}
@@ -65,29 +65,29 @@ const ProfileCard = ({ userData, activeTab }) => {
 
           <div className="flex flex-col gap-3">
             <p className="text-light-1 text-heading3-bold max-sm:text-heading4-bold">
-              {userData.firstName} {userData.lastName}
+              {userData?.firstName} {userData?.lastName}
             </p>
             <p className="text-light-3 text-subtle-semibold">
-              {userData.username}
+              {userData?.username}
             </p>
             <div className="flex gap-7 text-small-bold max-sm:gap-4">
               <div className="flex max-sm:flex-col gap-2 items-center max-sm:gap-0.5">
-                <p className="text-purple-1">{userData.posts.length}</p>
+                <p className="text-purple-1">{userData?.posts.length}</p>
                 <p className="text-light-1">Posts</p>
               </div>
               <div className="flex max-sm:flex-col gap-2 items-center max-sm:gap-0.5">
-                <p className="text-purple-1">{userData.followers.length}</p>
+                <p className="text-purple-1">{userData?.followers.length}</p>
                 <p className="text-light-1">Followers</p>
               </div>
               <div className="flex max-sm:flex-col gap-2 items-center max-sm:gap-0.5">
-                <p className="text-purple-1">{userData.following.length}</p>
+                <p className="text-purple-1">{userData?.following.length}</p>
                 <p className="text-light-1">Following</p>
               </div>
             </div>
           </div>
         </div>
 
-        {user.id !== userData.clerkId &&
+        {user.id !== userData?.clerkId &&
           (isFollowing ? (
             <PersonRemove
               sx={{ color: "#7857FF", cursor: "pointer", fontSize: "40px" }}
@@ -107,7 +107,7 @@ const ProfileCard = ({ userData, activeTab }) => {
             className={`tab ${
               activeTab === tab.name ? "bg-purple-1" : "bg-dark-2"
             }`}
-            href={`/profile/${userData._id}/${tab.link}`}
+            href={`/profile/${userData?._id}/${tab.link}`}
           >
             {tab.name}
           </Link>
